32882e1ce167cca67ab15c4e54f7f106cfd7e83f
TheFox <github@fox21.at>
2010-10-19 20:11:24 +0200

    No guests.

--- index.php
+++ index.php
@@ -6,43 +6,96 @@ define('ANTIHACK', 1);
 
 include_once('./lib/config.php');
 include_once('./lib/functions.php');
+include_once('./lib/class.user.php');
 
 
 $a = $_GET['a'];
 $id = (int)$_GET['id'];
 $smarty = smartyNew();
 
-switch($a){
-	
-	default:
+$user = new user(dbConnect());
+$user->loadBySessionId($_COOKIE['userSessionId']);
+
+
+if($user->isGuest){
+	switch($a){
 		
-		$tpl = 'default.tpl';
-		$cacheId = 'default';
-		if(!$smarty->isCached($tpl, $cacheId)){
-			smartyAssignStd($smarty);
+		default:
+		case 'login':
 			
+			$tpl = 'default-guest.tpl';
+			$cacheId = 'default-guest';
+			if(!$smarty->isCached($tpl, $cacheId)){
+				smartyAssignStd($smarty);
+				
+				
+			}
+			$smarty->display($tpl, $cacheId);
+			
+		break;
+		
+		case 'loginExec':
 			$dbh = dbConnect();
 			dbClose($dbh);
-			
-			$smarty->assign('content', '');
-			
-		}
-		$smarty->display($tpl, $cacheId);
+			//header('Location: ?');
+		break;
 		
-	break;
+	}
+}
+else{
 	
-	case 'dlpacketEdit':
+	// Only for non-loggedin users.
+	switch($a){
 		
-		$tpl = $a.'.tpl';
-		$cacheId = $a;
-		if(!$smarty->isCached($tpl, $cacheId)){
-			smartyAssignStd($smarty);
+		default:
 			
-			$dbh = dbConnect();
+			$tpl = 'default.tpl';
+			$cacheId = 'default';
+			if(!$smarty->isCached($tpl, $cacheId)){
+				smartyAssignStd($smarty);
+				
+				$dbh = dbConnect();
+				$packets = getDbTable($dbh, 'packets', "where _user = '".$user->id."'");
+				#$files = 
+				dbClose($dbh);
+				
+				$smarty->assign('content', '');
+				
+			}
+			$smarty->display($tpl, $cacheId);
 			
-			dbClose($dbh);
+		break;
+		
+		case 'dlpacketEdit':
 			
-			$filesOut = '';
+			$tpl = $a.'.tpl';
+			$cacheId = $a;
+			if(!$smarty->isCached($tpl, $cacheId)){
+				smartyAssignStd($smarty);
+				
+				$dbh = dbConnect();
+				dbClose($dbh);
+				
+				$filesOut = '';
+				if($id){
+					// Edit
+				}
+				else{
+					// Add new
+				}
+				
+				
+				$smarty->assign('id', $id);
+				$smarty->assign('files', $filesOut);
+				
+			}
+			$smarty->display($tpl, $cacheId);
+			
+			
+		break;
+		
+		case 'dlpacketEditSave':
+			
 			if($id){
 				// Edit
 			}
@@ -50,28 +103,11 @@ switch($a){
 				// Add new
 			}
 			
+			//header('Location: ?');
 			
-			$smarty->assign('id', $id);
-			$smarty->assign('files', $filesOut);
-			
-		}
-		$smarty->display($tpl, $cacheId);
+		break;
 		
-		
-	break;
-	
-	case 'dlpacketEditSave':
-		
-		if($id){
-			// Edit
-		}
-		else{
-			// Add new
-		}
-		
-		//header('Location: ?');
-		
-	break;
+	}
 	
 }
 
