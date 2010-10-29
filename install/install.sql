bfa76586ad320a12672efc7a7439eae368552c72
TheFox <github@fox21.at>
2010-10-29 21:35:25 +0200

    SQL struct auto export for install.

--- install/install.sql
+++ install/install.sql
@@ -1,4 +1,4 @@
--- Created @ 2010/10/29 20:42:37 by TheFox@fox21.at
+-- Created @ 2010/10/29 21:35:24 by TheFox@fox21.at
 -- MySQL dump 10.13  Distrib 5.1.45, for debian-linux-gnu (i486)
 --
 -- Host: localhost    Database: phpdl
@@ -97,7 +97,7 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2010-10-29 20:42:39
+-- Dump completed on 2010-10-29 21:35:25
 -- MySQL dump 10.13  Distrib 5.1.45, for debian-linux-gnu (i486)
 --
 -- Host: localhost    Database: phpdl
@@ -154,4 +154,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2010-10-29 20:42:39
+-- Dump completed on 2010-10-29 21:35:25
