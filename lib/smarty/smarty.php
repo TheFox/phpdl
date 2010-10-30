73136b1cfb4f5c1575d99405baeccf5a5baf1cb8
TheFox <github@fox21.at>
2010-10-30 20:47:42 +0200

    Smarty include alias.

---
+++ lib/smarty/smarty.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+	Created @ 30.10.2010 by TheFox@fox21.at
+	Copyright (c) 2010 TheFox
+	
+	This file is part of PHPDL.
+	
+	PHPDL is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+	
+	PHPDL is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with PHPDL.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+include_once('Smarty-3.0rc4/Smarty.class.php');
+
+?>
