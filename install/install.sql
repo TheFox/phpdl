250fdd2bd6061542fcfe3c26321254e4ebb7f5c4
TheFox <github@fox21.at>
2010-11-02 21:38:40 +0100

    SQL struct auto export for install.

--- install/install.sql
+++ install/install.sql
@@ -1,1 +1,157 @@
--- Created @ 2010/11/02 21:37:28 by TheFox@fox21.at
+-- Created @ 2010/11/02 21:38:38 by TheFox@fox21.at
+-- MySQL dump 10.13  Distrib 5.1.45, for debian-linux-gnu (i486)
+--
+-- Host: localhost    Database: phpdl
+-- ------------------------------------------------------
+-- Server version	5.1.45-1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `files`
+--
+
+DROP TABLE IF EXISTS `files`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `files` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `_user` int(11) NOT NULL,
+  `_packet` int(11) NOT NULL,
+  `uri` text NOT NULL,
+  `md5` varchar(32) NOT NULL,
+  `md5Verified` enum('0','1') NOT NULL DEFAULT '0',
+  `size` bigint(20) NOT NULL DEFAULT '0',
+  `error` smallint(4) NOT NULL DEFAULT '0',
+  `ctime` int(11) NOT NULL DEFAULT '0',
+  `stime` int(11) NOT NULL DEFAULT '0',
+  `ftime` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `_user` (`_user`),
+  KEY `_packet` (`_packet`),
+  KEY `stime` (`stime`),
+  KEY `ftime` (`ftime`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `packets`
+--
+
+DROP TABLE IF EXISTS `packets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `packets` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `_user` int(11) NOT NULL,
+  `name` varchar(256) NOT NULL,
+  `archive` enum('0','1') NOT NULL DEFAULT '0',
+  `md5Verified` enum('0','1') NOT NULL DEFAULT '0',
+  `source` text NOT NULL,
+  `password` text NOT NULL,
+  `ctime` int(11) NOT NULL DEFAULT '0',
+  `stime` int(11) NOT NULL DEFAULT '0',
+  `ftime` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `_user` (`_user`),
+  KEY `archive` (`archive`),
+  KEY `stime` (`stime`),
+  KEY `ftime` (`ftime`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(32) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `sessionId` varchar(32) NOT NULL COMMENT 'md5 hex',
+  `ctime` int(11) NOT NULL DEFAULT '0' COMMENT 'Create time',
+  PRIMARY KEY (`id`),
+  KEY `login` (`login`),
+  KEY `sessionId` (`sessionId`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2010-11-02 21:38:40
+-- MySQL dump 10.13  Distrib 5.1.45, for debian-linux-gnu (i486)
+--
+-- Host: localhost    Database: phpdl
+-- ------------------------------------------------------
+-- Server version	5.1.45-1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `hosters`
+--
+
+DROP TABLE IF EXISTS `hosters`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `hosters` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) NOT NULL,
+  `phpPath` varchar(256) NOT NULL,
+  `searchPattern` varchar(256) NOT NULL,
+  `ssl` enum('0','1') NOT NULL DEFAULT '0',
+  `user` varchar(256) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `ctime` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hosters`
+--
+
+LOCK TABLES `hosters` WRITE;
+/*!40000 ALTER TABLE `hosters` DISABLE KEYS */;
+INSERT INTO `hosters` VALUES (1,'RapidShare.com','hoster.rapidshare-com.php','rapidshare.com\\/files\\/','1','','',1287612321);
+/*!40000 ALTER TABLE `hosters` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2010-11-02 21:38:40
